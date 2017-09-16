612c438c78e583af052b1fc0b3ac26c12d003f08
thartman77 <monker36@gmail.com>
2017-09-16 16:21:42 -0400

    Set up server for markov/twitter module

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
-@app.route('/_add_numbers')
-def add_numbers():
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
+@app.route('/_get_tweet')
+def get_tweet():
+    handle = request.args.get('handle', '', type=str)
+    return jsonify(result=get_markov(handle))
 
 @app.route('/')
 def index():
