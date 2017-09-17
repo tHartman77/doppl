9f65dc8506a76d5c4223ca10b9060f2587b6173c
thartman77 <monker36@gmail.com>
2017-09-16 22:30:56 -0400

    Add basic twitter template

--- markovtest.py
+++ markovtest.py
@@ -47,7 +47,6 @@ def get_all_tweets(screen_name):
 		outtweets[tweet]= re.sub(r"http\S+", "somelink.com",outtweets[tweet])
 		if(outtweets[tweet].startswith("RT")):
 			outtweets[tweet] = ""
-		print(outtweets[tweet])
 	return outtweets
 
 def get_markov_tweet(screen_name):
@@ -63,5 +62,3 @@ def get_markov_tweet(screen_name):
 
 	# Print three randomly-generated sentences of no more than 140 characters
     return text_model.make_short_sentence(140)
-
-get_all_tweets("billlaboon")
--- static/script.js
+++ static/script.js
@@ -3,7 +3,8 @@ var submit_form = function(e){
   $.getJSON($SCRIPT_ROOT + '/_get_tweet', {
     handle: $('input[name="handle"]').val(),
   }, function(data) {
-    $('#result').text(data.result);
+    make_tweet_ui(data.result);
+    //$('#result').text(data.result);
     $('input[name=handle]').focus().select();
   });
   return false;
@@ -15,4 +16,30 @@ $('input[type=text]').bind('keydown', function(e) {
   }
 });
 $('input[name=handle]').focus();
+
+function make_tweet_ui(tweet_text) {
+    console.log("hi");
+    twttr.widgets.createTweet(
+      
+      // Replace this with the Tweet ID
+      '909216885530558464', document.getElementById("tweet"))
+      .then(function(el) {
+
+        var e = el.contentDocument;
+
+        // Change the tweet text
+        var html = e.querySelector(".Tweet-text");
+        html.innerHTML = tweet_text;
+
+        // Change the retweet count
+        e.querySelector(".TweetAction--retweet .TweetAction-stat").innerHTML = "123";
+
+        // Change the favorites count
+        e.querySelector(".TweetAction--favorite .TweetAction-stat").innerHTML = "999";
+
+        // Replace the date with text
+        e.querySelector(".dt-updated").innerHTML = "Contact the author of this tweet at amit@labnol.org";
+        
+    });
+}
 });
